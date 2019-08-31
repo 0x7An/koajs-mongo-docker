d66fc7fbfb79457ddabee2e85e869d8be6bd9509
Anderson <andersons.code@gmail.com>
2019-08-30 21:47:47 -0300

    feat: userModel and mongoose object

---
+++ src/infra/database/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const UsersModel = mongoose.model('User', UserSchema);
---
+++ src/infra/database/mongoose.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+import { logger } from '../logger'
+import { env } from '../env'
+import { UsersModel } from './models/userModel'
+
+mongoose.connect(env.MONGO_URI, { useNewUrlParser: true }).then(() => {
+  logger.info(`Successful connected to mongo on ${env.MONGO_URI}`, { scope: 'startup' })
+
+}).catch(err => {
+    logger.error(`Error while connectting to mongo on ${env.MONGO_URI}`, err, { scope: 'startup' })
+    process.exit(1)
+})
+
+export default mongoose
