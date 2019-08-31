ab7d97cd74afaee822897d0243136590127c9df1
Anderson <andersons.code@gmail.com>
2019-08-30 21:48:34 -0300

    feat: controllers and routes

---
+++ src/app/controllers/userController.js
@@ -0,0 +1,42 @@
+'use strict'
+import { userService } from '../../domain/user/userService'
+import { isEmail } from '../helpers'
+
+const handleResponse = async (ctx, callback) => {
+    let { status, data } = await callback    
+    ctx.status = status
+    ctx.body = data ? data : { "error" : "Internal server error"}
+}
+
+export const authenticate = async (ctx, next) => {
+  const { email, password } = ctx.request.body
+  await handleResponse(ctx, userService.authenticate({ email, password }))
+}
+
+export const index = async (ctx, next) => {
+    await handleResponse(ctx, userService.getAllUsers())
+}
+
+export const find = async (ctx, next) => {
+    const { id } = ctx.params;
+    if (isEmail(id)){
+        await handleResponse(ctx, userService.getUserByEmail(id))
+    } else {
+        await handleResponse(ctx, userService.getUserById(id))
+    }
+}
+
+export const create = async (ctx, next) => {
+    const { name, email, password } = ctx.request.body
+    await handleResponse(ctx, userService.createUser({ name, email, password }))
+}
+
+export const update = async (ctx, next) => {
+    const { id, name, email, password } = ctx.request.body
+    await handleResponse(ctx, userService.updateUser({ id, name, email, password }))
+}
+
+export const destroy = async (ctx, next) => {
+    const { id } = ctx.request.body
+    await handleResponse(ctx, userService.destroy(id))
+}
--- src/app/routes.js
+++ src/app/routes.js
@@ -1,8 +1,19 @@
-import  Router from 'koa-router';
-var router = new Router();
+'use strict'
+
+import Router from 'koa-router';
+import { authenticate, index, create, find, update, destroy } from './controllers';
+const router = new Router();
 
 router.get('/', (ctx, next) => {
-    ctx.body = 'Hello World!';
+    ctx.body = 'Accounts Service';
 });
+
+router.post('/register', create);
+router.post('/authenticate', authenticate);
+
+router.get('/users', index);
+router.get('/user/:id', find);
+router.put('/user', update);
+router.delete('/user', destroy);
 
 export default router
