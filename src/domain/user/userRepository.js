cf0a65aa5d1f3bd08a4e12559727f200373a07bd
Anderson <andersons.code@gmail.com>
2019-08-30 21:47:58 -0300

    feat: users repository

---
+++ src/domain/user/userRepository.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+import { logger } from '../../infra/logger'
+const User = mongoose.model('User')
+
+const handleMongoQuery = async (queryCallback) => {
+  try {
+    let result = await queryCallback;
+    return result 
+  } catch (error) {
+    logger.error(error, { scope: 'Mongo Repository' })
+    return null;
+  }
+}
+
+export const usersRepository = {
+    getAllUsers : async () => handleMongoQuery(User.find({})),
+    getUserById : async (id) => handleMongoQuery(User.findById(id)),
+    getUserByEmail: async (email) => handleMongoQuery(User.findOne({ "email": email })),
+    createUser: async (userData) => handleMongoQuery(User.create(userData)),
+    updateUser: async ({ id, name, email, password }) => handleMongoQuery(User.findByIdAndUpdate(id, { name, email, password }, { new: true })),
+    destroy: async (id) => handleMongoQuery(User.findByIdAndRemove(id))
+  };
