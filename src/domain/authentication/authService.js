bc8fc6ee350935ce34261880a58ddceaccde9a39
Anderson <andersons.code@gmail.com>
2019-08-30 21:48:16 -0300

    feat: auth service

---
+++ src/domain/authentication/authService.js
@@ -0,0 +1,26 @@
+import { Service } from '../core/service'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { env } from '../../infra/env'
+
+class AuthService extends Service {
+  constructor() {
+    super();
+  }
+
+  async comparePassword(passwordGiven, passwordStored) {
+    return await bcrypt.compare(passwordGiven, passwordStored);
+  }
+
+  async hashPassword(passwordGiven) {
+    return await bcrypt.hash(passwordGiven, 8);
+  }
+
+  async generateToken(id) {
+    return jwt.sign({ id: id }, env.PASSWORD_SECRET, {
+        expiresIn: 86400, // expires in 1 day
+      });
+  }
+}
+
+export const authService = new AuthService();
